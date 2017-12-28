61438c2338515b59a049e461b789e718a6644aa0
Ubuntu64 <dihui.wang@transwarp.io>
2017-12-28 04:06:52 -0800

    feat: optimization of chart donut

--- src/app/chart/lib/helpers/donut-chart-builder.ts
+++ src/app/chart/lib/helpers/donut-chart-builder.ts
@@ -13,46 +13,116 @@ export class DonutChartDataBuilder {
 
 mockData = {
 
-  columns: [
-    'Under 5 Years', '5 to 13 Years', '14 to 17 Years', '18 to 24 Years',
-    '25 to 44 Years', '45 to 64 Years', '65 Years and Over',
-  ],
-
   donuts: [
     {
+      state: 'AL',
+      // columns?: [],
+      parts: [
+        {
+          'title': 'Under 5 Years',
+          'value': 310504,
+        },
+        {
+          'title': '5 to 13 Years',
+          'value': 552339,
+        },
+        {
+          'title': '14 to 17 Years',
+          'value': 259034,
+        },
+        {
+          'title': '18 to 24 Years',
+          'value': 450818,
+        },
+        {
+          'title': '25 to 44 Years',
+          'value': 1231572,
+        },
+        {
+          'title': '45 to 64 Years',
+          'value': 1215966,
+        },
+        {
+          'title': '65 Years and Over',
+          'value': 641667,
+        },
+      ],
+    },
+    {
+      state: 'Bl',
+      // columns?: [],
+      parts: [
+        {
+          'title': 'Under 5 Years',
+          'value': 310504,
+        },
+        {
+          'title': '5 to 13 Years',
+          'value': 552339,
+        },
+        {
+          'title': '14 to 17 Years',
+          'value': 379034,
+        },
+        {
+          'title': '18 to 24 Years',
+          'value': 450818,
+        },
+        {
+          'title': '25 to 44 Years',
+          'value': 1231572,
+        },
+        {
+          'title': '45 to 64 Years',
+          'value': 2015966,
+        },
+        {
+          'title': '65 Years and Over',
+          'value': 641667,
+        },
+      ],
+    },
+  ],
+
+  /*donuts: [
+    {
+      columns: [
+        'Under 5 Years', '5 to 13 Years', '14 to 17 Years', '18 to 24 Years',
+        '25 to 44 Years', '45 to 64 Years', '65 Years and Over',
+      ],
       'state': 'AL',
       'sum': 4661900,
       'ages': [
         {
           'age': 'Under 5 Years',
-          'population': 310504,
+          'value': 310504,
         },
         {
           'age': '5 to 13 Years',
-          'population': 552339,
+          'value': 552339,
         },
         {
           'age': '14 to 17 Years',
-          'population': 259034,
+          'value': 259034,
         },
         {
           'age': '18 to 24 Years',
-          'population': 450818,
+          'value': 450818,
         },
         {
           'age': '25 to 44 Years',
-          'population': 1231572,
+          'value': 1231572,
         },
         {
           'age': '45 to 64 Years',
-          'population': 1215966,
+          'value': 1215966,
         },
         {
           'age': '65 Years and Over',
-          'population': 641667,
+          'value': 641667,
         },
       ],
     },
-  ],
+  ],*/
 };
 
