d9c2b200c49de0734f054a71dae54560682e016a
rockwang <rockwang@localhost.localdomain>
2018-11-08 11:09:51 +0800

    WARP-28488：[TDC-ECO] 主机列表部分相关

--- src/app/account/license/upload/upload.component.spec.ts
+++ src/app/account/license/upload/upload.component.spec.ts
@@ -1,9 +1,9 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TuiModalRef, TUI_MODAL_DATA, TuiModalService } from 'tdc-ui';
-
 import { UploadComponent } from './upload.component';
 import { TranslateService } from '../../../i18n/translate.service';
 import { TuiMessageService } from 'tdc-ui';
@@ -17,6 +17,7 @@ describe('UploadComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
       imports: [
         HttpClientModule,
         OverlayModule,
