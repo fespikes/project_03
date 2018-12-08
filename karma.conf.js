6628f18b29f864a354dfe389af315f0efe969032
rockwang <rockwang@localhost.localdomain>
2018-12-08 11:15:31 +0800

    proxy of unit test

--- karma.conf.js
+++ karma.conf.js
@@ -26,6 +26,13 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    proxies: {
+      '/api/': {
+        'target': 'http://172.16.132.47:9099/',
+        'secure': false,
+        'changeOrigin': true
+      }
+    }
   });
 };
